dd4cb0b31f75b297f99cd9a1535d18b05993689f
demola malomo <malomo.alaba@yahoo.com>
2022-01-30 00:31:30 +0100

    env config and helper starter

---
+++ configs/env.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func EnvCloudName() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	return os.Getenv("CLOUDINARY_CLOUD_NAME")
+}
+
+func EnvCloudAPIKey() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	return os.Getenv("CLOUDINARY_API_KEY")
+}
+
+func EnvCloudAPISecret() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	return os.Getenv("CLOUDINARY_API_SECRET")
+}
